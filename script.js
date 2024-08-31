83d9b76bed02ee515e423696117c19f3bf9a1fd0
Artem <artembaa8@gmail.com>
2024-08-31 17:40:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let age = 13;
+console.log(age)
+let name = ("sigma")
+console.log(name);
+let isStudent = false;
+console.log (isStudent);
+let myString = ("Три слова- это два слова. Джейсон Стетхем (с)")
+console.log(myString)
+let myNumber = 15;
+myNumber = 15+10;
+console.log(myNumber)
+const myNull = null;
+console.log(myNull)
+let username = prompt("Введіть ваше ім'я")
+alert("Привіт," + username )
+let accept = confirm("списать все деньги с карты?")
+if (accept) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+alert("ця дія небезпечна!")
+let userconfirm = confirm ("ви підтверджуєте дію?")
+if (userconfirm) {
+    alert("дякую за підтвердження!");
+} else {
+    alert("дію відмінено!");
+}
